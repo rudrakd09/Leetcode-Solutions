e5d67d4e032ce6b9721200e067fe2300c0815acf
rudrakd09 <149081336+rudrakd09@users.noreply.github.com>
2026-01-12 21:46:13 +0530

    Numbers smaller than current number solution

---
+++ Numbers_smallerthancurrentnumber.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> result;
+        for(int i=0; i<nums.size(); i++){
+            int count = 0;
+            for(int j =0; j<nums.size(); j++){
+                if(nums[j]<nums[i]){
+                    count++;
+                }
+            }
+            result.push_back(count);
+        }
+        return result;
+    }
+};
+int main(){
+
+    return 0;
+}
