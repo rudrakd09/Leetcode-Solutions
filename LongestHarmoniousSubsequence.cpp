e9b5c77d1e11311b77e968bc0be51190f11686e9
rudrakd09 <149081336+rudrakd09@users.noreply.github.com>
2026-01-31 21:45:29 +0530

    Leetcode Solutions

---
+++ LongestHarmoniousSubsequence.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        int ans = 0;
+        for (auto &it : freq) {
+            int x = it.first;
+            if (freq.count(x + 1)) {
+                ans = max(ans, freq[x] + freq[x + 1]);
+            }
+        }
+        return ans;
+    }
+};
+int main(){
+
+    return 0;
+}
